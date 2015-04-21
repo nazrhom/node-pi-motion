756d990631a47cd53938b581ef504a185420e128
Giovanni Garufi <giovanni.garufi@fwr.it>
2015-04-21 17:14:39 +0200

    Fix typo

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 var child_process = require('child_process');
 
-child_process.exec('./pi-motion-lite.py', function(error, stdout, stderr){
-			if (error) {
-	          console.log(error);
-	          process.exit(1);
-	        }
-	        if(stderr) {
-	          console.log(stderr);
-	        }
-	        if (stdout) {
-            console.log(stdout)
-          }
-      });
-	});
+child_process.exec('./pi-motion-lite.py', function(error, stdout, stderr) {
+  if (error) {
+    console.log(error);
+    process.exit(1);
+  }
+  if(stderr) {
+    console.log(stderr);
+  }
+  if (stdout) {
+    console.log(stdout)
+  }
 });
