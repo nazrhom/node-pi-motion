c8379b4b8b73d50bf91d35961ed8045c79388b6c
Giovanni Garufi <giovanni.garufi@fwr.it>
2015-04-23 15:20:14 +0200

    Add autorestart

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var DEBUG = 'node-pi-motion';
 var kill = function (pid, signal) {
   // Follow default node behavior
   signal = signal || 'SIGKILL';
-  
+
   psTree(pid, function(err, children) {
     var pids = [pid].concat(_.map(children, _.partialRight(_.pick, 'PID')));
     _.forEach(pids, function(tpid) {
@@ -45,6 +45,7 @@ function NodePiMotion(opts) {
 
   this.throttle = opts.throttle || 0;
   this.autorestart = opts.autorestart || false;
+  this.verbose = opts.verbose || false;
 
   this.emitMessage = _.throttle(function() {
     self.emit('DetectedMotion');
@@ -54,7 +55,7 @@ function NodePiMotion(opts) {
 
   var pythonArgs = buildPythonArgs(_.pick(opts, 'threshold', 'sensitivity', 'night'));
 
-  var pyOptions = {
+  this.pyOptions = {
     mode: 'text',
     pythonPath: opts.pythonPath || '/usr/bin/python',
     pythonOptions: ['-u'],
@@ -62,30 +63,41 @@ function NodePiMotion(opts) {
     args: pythonArgs
   };
 
-  this.pythonChild = new PythonShell('pi-motion-lite.py', pyOptions);
+  this.pythonChild = new PythonShell('pi-motion-lite.py', this.pyOptions);
 
-  this.pythonChild.on('message', function (message) {
-    if (opts.verbose) console.log(DEBUG, message);
+  this.attachListeners();
+}
+
+util.inherits(NodePiMotion, EventEmitter);
+
+NodePiMotion.prototype.attachListeners = function () {
+  var self = this;
+
+  self.pythonChild.on('message', function (message) {
     if (message === 'DetectedMotion') {
       self.emitMessage();
-    }
+    } else if (self.verbose) console.log(DEBUG, message);
+
   });
 
-  this.pythonChild.on('error', function(err) {
-    if (opts.verbose) console.log(DEBUG, 'Python script errored with error: ', err);
-    
-    // The error event is fired on non 0 exit or when writing to stderr so we want to make sure the 
+  self.pythonChild.on('error', function(err) {
+    if (self.verbose) console.log(DEBUG, 'Python script errored with error: ', err);
+
+    // The error event is fired on non 0 exit or when writing to stderr so we want to make sure the
     // script has actually exited before eventually restarting it
     if (!self.pythonChild.terminated) {
-      if (opts.verbose) console.log(DEBUG, 'Killing python script...');
+      if (self.verbose) console.log(DEBUG, 'Killing python script...');
       kill(self.pythonChild.pid, 'SIGTERM')
     }
   });
 
-  this.pythonChild.on('close', function() {
-    if (opts.verbose) console.log(DEBUG, 'Python Script has exited');
+  self.pythonChild.on('close', function() {
+    if (self.verbose) console.log(DEBUG, 'Python Script has exited');
+    if (self.autorestart) {
+      self.pythonChild = new PythonShell('pi-motion-lite.py', self.pyOptions);
+      self.attachListeners();
+    }
   });
 }
 
-util.inherits(NodePiMotion, EventEmitter);
 module.exports = NodePiMotion;
